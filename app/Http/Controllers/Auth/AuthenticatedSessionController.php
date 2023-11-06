5bc297755f12e113f8af0a0ed3b128362ad79f54
Julian-root <adjibiadechinan@gmail.com>
2023-11-06 13:50:20 +0100

    Auth

---
+++ app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\Auth\LoginRequest;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Auth\AuthenticationException;
+use Illuminate\Validation\ValidationException;
+
+class AuthenticatedSessionController extends Controller
+{
+    /**
+     * Handle an incoming authentication request.
+     */
+    public function store(LoginRequest $request)
+    {
+        try {
+            $request->authenticate();
+            $request->session()->regenerate();
+            $user = User::where('email', $request->email)->first();
+            $token = $user->createToken('myapptoken')->plainTextToken;
+            return self::apiResponse(true, "Connexion reussie", [$user, $token]);
+        } catch (ValidationException $e) {
+            return self::apiResponse(false, "Échec de la connexion");
+        } catch (AuthenticationException $e) {
+            return self::apiResponse(false, "Échec de la connexion : identifiants incorrects");
+        }
+    }
+
+    /**
+     * Destroy an authenticated session.
+     */
+    public function destroy(Request $request): Response
+    {
+        Auth::guard('web')->logout();
+
+        $request->session()->invalidate();
+
+        $request->session()->regenerateToken();
+
+        return response()->noContent();
+    }
+
+    public static function apiResponse($success, $message, $data = [], $status = 200) //: array
+    {
+        $response = response()->json([
+            'success' => $success,
+            'message' => $message,
+            'body' => $data
+        ], $status);
+        return $response;
+    }
+}
---
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Auth\Events\Registered;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Validation\Rules;
+use Illuminate\Validation\ValidationException;
+
+class RegisteredUserController extends Controller
+{
+    /**
+     * Handle an incoming registration request.
+     *
+     * @throws \Illuminate\Validation\ValidationException
+     */
+    public function store(Request $request)
+    {
+        try {
+            $request->validate([
+                'name' => ['required', 'string', 'max:255'],
+                'email' => ['required', 'string', 'lowercase', 'email', 'max:255', 'unique:'.User::class],
+                'password' => ['required', 'confirmed', Rules\Password::defaults()],
+            ]);
+
+            $user = User::create([
+                'name' => $request->name,
+                'email' => $request->email,
+                'password' => Hash::make($request->password),
+            ]);
+
+            event(new Registered($user));
+
+            Auth::login($user);
+            $token = $user->createToken('myapptoken')->plainTextToken;
+                return self::apiResponse(true, "Inscription reussie", [$user, $token]);
+        } catch (ValidationException $e) {
+                return self::apiResponse(false, "Échec de l inscription");
+        }
+    }
+
+    public static function apiResponse($success, $message, $data = [], $status = 200) //: array
+    {
+        $response = response()->json([
+            'success' => $success,
+            'message' => $message,
+            'body' => $data
+        ], $status);
+        return $response;
+    }
+}
