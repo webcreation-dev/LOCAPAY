0e9001d3fa8c1eb41e7651e62e7c9395a12a529c
Eden <aedenrosario@gmail.com>
2023-11-17 12:29:26 +0100

    refractor_controller_and_add_reasaon_in_parameter

--- app/Http/Controllers/MtnMobileMoneyController.php
+++ app/Http/Controllers/MtnMobileMoneyController.php
@@ -14,15 +14,30 @@ class MtnMobileMoneyController extends Controller
         $this->collection = new Collection();
     }
 
-    public function initiatePayment($amount, $payerMobileNumber, $currency)
+    /**
+     * Initiates a payment.
+     *
+     * @param mixed $amount The amount of the payment.
+     * @param mixed $payerMobileNumber The mobile number of the payer.
+     * @param mixed $reason The reason for the payment.
+     *
+     * @return JsonResponse The JSON response containing the reference ID.
+     */
+    public function initiatePayment($amount, $payerMobileNumber, $reason)
     {
-        // Step 1: Create API User, Create API Key, and Request to Pay
-        $referenceId = $this->collection->requestToPay('testPayment', $payerMobileNumber, $amount);
+        // Request the payment from the payment collection service
+        $referenceId = $this->collection->requestToPay($reason, $payerMobileNumber, $amount);
 
-        // Handle the payment response as needed
+        // Return a JSON response with the reference ID
         return response()->json(['reference_id' => $referenceId]);
     }
 
+    /**
+     * Retrieves the payment status for a given reference ID.
+     *
+     * @param int $referenceId The reference ID of the transaction.
+     * @return \Illuminate\Http\JsonResponse The JSON response containing the status of the transaction.
+     */
     public function getPaymentStatus($referenceId)
     {
         $status = $this->collection->getTransactionStatus($referenceId);
