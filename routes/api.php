3e206a66db35642cb2ad6b94911eb288743d564e
AGOSSADOU Elognisse Erasme Guérin <55337134+agossadourin@users.noreply.github.com>
2024-06-10 10:55:08 +0100

    fix bug

--- routes/api.php
+++ routes/api.php
@@ -36,30 +36,32 @@ Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
 Route::group(['middleware' => ['auth:sanctum']], function() {
 
 
-    Route::resource('contracts', ContractController::class);
+    // Route::resource('contracts', ContractController::class);
+    // Route::resource('notifications', NotificationController::class);
+    // Route::resource('transactions', TransactionController::class);
+    // Route::resource('schedules', ScheduleController::class);
+    // Route::get('/initiate-transaction/{amount}/{reason}/{type}', [MtnMobileMoneyController::class, 'initiateTransaction']);
+    // Route::get('/action-transaction', [TransactionController::class, 'actionTransaction'])->name('action-transaction');
+    // Route::get('/payment-transaction/{amount}/{reason}/{type}/{phone}', [MtnMobileMoneyController::class, 'paymentTransaction']);
+    // Route::get('/transfert-transaction', [TransactionController::class, 'transfertTransaction'])->name('transfert-transaction');
 
-    Route::resource('notifications', NotificationController::class);
-
-    Route::resource('transactions', TransactionController::class);
-
-    Route::resource('schedules', ScheduleController::class);
+    Route::post('/logout', [AuthenticatedSessionController::class, 'destroy']);
 
     Route::resource('visits', VisitController::class);
 
     Route::get('/get_properties_by_owner', [PropertyController::class, 'getPropertiesByOwner']);
 
-    Route::get('/initiate-transaction/{amount}/{reason}/{type}', [MtnMobileMoneyController::class, 'initiateTransaction']);
-    Route::get('/action-transaction', [TransactionController::class, 'actionTransaction'])->name('action-transaction');
+    // Route::resource('properties', PropertyController::class);
 
-    Route::get('/payment-transaction/{amount}/{reason}/{type}/{phone}', [MtnMobileMoneyController::class, 'paymentTransaction']);
-    Route::get('/transfert-transaction', [TransactionController::class, 'transfertTransaction'])->name('transfert-transaction');
-
-    Route::post('/logout', [AuthenticatedSessionController::class, 'destroy']);
-
-    Route::resource('properties', PropertyController::class);
+    Route::get('properties/create', [PropertyController::class, 'create'])->name('properties.create');
+    Route::post('properties', [PropertyController::class, 'store'])->name('properties.store');
+    Route::get('properties/{property}/edit', [PropertyController::class, 'edit'])->name('properties.edit');
+    Route::put('properties/{property}', [PropertyController::class, 'update'])->name('properties.update');
+    Route::delete('properties/{property}', [PropertyController::class, 'destroy'])->name('properties.destroy');
 });
 
-Route::resource('locations', PropertyController::class);
+Route::get('properties', [PropertyController::class, 'index'])->name('properties.index');
+Route::get('properties/{property}', [PropertyController::class, 'show'])->name('properties.show');
 
 
 // CONFIGURATION
