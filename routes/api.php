d60be54ef303256f6297d691fc90b2701d978d39
AGOSSADOU Elognisse Erasme Guérin <55337134+agossadourin@users.noreply.github.com>
2024-06-10 14:11:47 +0100

    fix bug

--- routes/api.php
+++ routes/api.php
@@ -53,15 +53,14 @@ Route::group(['middleware' => ['auth:sanctum']], function() {
 
     // Route::resource('properties', PropertyController::class);
 
-    Route::get('properties/create', [PropertyController::class, 'create'])->name('properties.create');
     Route::post('properties', [PropertyController::class, 'store'])->name('properties.store');
-    Route::get('properties/{property}/edit', [PropertyController::class, 'edit'])->name('properties.edit');
     Route::put('properties/{property}', [PropertyController::class, 'update'])->name('properties.update');
     Route::delete('properties/{property}', [PropertyController::class, 'destroy'])->name('properties.destroy');
-});
 
 Route::get('properties', [PropertyController::class, 'index'])->name('properties.index');
 Route::get('properties/{property}', [PropertyController::class, 'show'])->name('properties.show');
+
+});
 
 
 // CONFIGURATION
